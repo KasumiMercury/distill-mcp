14581ae31e4f92e9fdd0956cade8b4fa13c84435
KasumiMercury <naoki_shira@outlook.jp>
2025-04-06 20:10:28 +0900

    chore: update .gitignore to include dist directory

--- dist/distill/distillDocumentFromURL.js
+++
@@ -1,8 +0,0 @@
-import { JSDOM } from "jsdom";
-export const distillDocumentFromURL = async (url) => {
-    const dom = await JSDOM.fromURL(url);
-    const document = dom.window.document;
-    // TODO: impl distill
-    // TODO: convert to Markdown
-    return document.title;
-};
