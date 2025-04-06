faa722851cdfe8a2ea937941ccfbaecaf3a039e8
KasumiMercury <naoki_shira@outlook.jp>
2025-04-06 18:44:38 +0900

    feat: mcp server core

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,43 @@
 import {distillDocumentFromURL} from "./distill/distillDocumentFromURL.js";
+import {McpServer} from '@modelcontextprotocol/sdk/server/mcp.js'
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 
-const args = process.argv.slice(2)
-const targetUrl = args[0]
+const server = new McpServer({
+	name: "distill-mcp",
+	version: "0.0.1",
+	capabilities: {
+		resources: {},
+		tools: {},
+	},
+})
 
-if (!targetUrl) {
-	console.error('Please provide a URL as an argument.')
-	process.exit(1)
+// const args = process.argv.slice(2)
+// const targetUrl = args[0]
+//
+// if (!targetUrl) {
+// 	console.error('Please provide a URL as an argument.')
+// 	process.exit(1)
+// }
+//
+// distillDocumentFromURL(targetUrl)
+// 	.then((result) => {
+// 		console.log('Distilled Document:', result)
+// 		console.log('Done!')
+// 	})
+// 	.catch((error) => {
+// 		console.error('Error:', error)
+// 	})
+// 	.finally(() => {
+// 		process.exit(0)
+// 	})
+
+async function main() {
+	const transport = new StdioServerTransport();
+	await server.connect(transport);
+	console.error("Weather MCP Server running on stdio");
 }
 
-distillDocumentFromURL(targetUrl)
-	.then((result) => {
-		console.log('Distilled Document:', result)
-		console.log('Done!')
-	})
-	.catch((error) => {
-		console.error('Error:', error)
-	})
-	.finally(() => {
-		process.exit(0)
-	})
+main().catch((error) => {
+	console.error("Fatal error in main():", error);
+	process.exit(1);
+});
