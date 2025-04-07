92d22b7a62755cfed6628440bbb991898a64ee22
KasumiMercury <naoki_shira@outlook.jp>
2025-04-07 22:16:54 +0900

    feat: implement document distillation and convert to Markdown

--- src/distill/distillDocumentFromURL.ts
+++ src/distill/distillDocumentFromURL.ts
@@ -1,11 +1,28 @@
 import {JSDOM} from "jsdom";
+import {Readability} from "@mozilla/readability";
+import DOMPurify from "dompurify";
+import TurndownService from "turndown";
 
 
 export const distillDocumentFromURL = async (url: string): Promise<string> => {
 	const dom = await JSDOM.fromURL(url)
 	const document = dom.window.document
 
-	// TODO: impl distill
-	// TODO: convert to Markdown
-	return document.title
+	if (!document) {
+		throw new Error("Failed to parse document");
+	}
+
+	const article = new Readability(document).parse();
+
+	if (!article) {
+		throw new Error("Failed to parse article");
+	}
+
+	if (!article.content) {
+		throw new Error("Failed to get article content");
+	}
+
+	const turndownService = new TurndownService();
+	const sanitizedContent = DOMPurify.sanitize(article.content);
+	return turndownService.turndown(sanitizedContent);
 }
